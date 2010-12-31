856286c0e6618f4369da3ebfc562d63d5eb37426
Robbie McMahon <robbie@Mega-Oge.local>
2010-12-30 16:19:55 -0800

    Creating TriObjects now works.

--- TriSelect/TriSelect.hs
+++ TriSelect/TriSelect.hs
@@ -6,11 +6,18 @@ module Main (
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
 
+-- For exit menu
 import Graphics.UI.GLUT.Menu
-
 import System.Exit
 
+-- For callbacks
 import TriSelect_Display (display, idle)
+
+-- For trianlge objects
+import Tri_Objects
+
+-- Random number generation
+import Random
 
 exitLoop = exitSuccess
 
@@ -24,7 +31,14 @@ main = do
   displayCallback $= display
   idleCallback $= Just display
   
-  -- This does not work with "runhaskell". It needs to be compiled with the C wrapper.
+  -- This does not work with "runhaskell". It needs to be compiled.
   attachMenu RightButton (Menu [MenuEntry "Exit" exitLoop])
   
   mainLoop
+  
+initObjects :: StdGen -> Int -> [TriObject]
+initObjects gen numObjects  
+  | numObjects > 0 = (randomObject gen1):(initObjects gen2 (numObjects-1))
+  | otherwise = []
+  where
+    (gen1, gen2) = split gen
--- TriSelect/Tri_Objects.hs
+++ TriSelect/Tri_Objects.hs
@@ -1,3 +1,35 @@
 module Tri_Objects where
 
-data TriObject = TriObject {v1=
+import Graphics.Rendering.OpenGL
+import Data.IORef
+
+import Random
+
+data TriObject = TriObject {v1 :: Vertex2 GLfloat,
+                            v2 :: Vertex2 GLfloat,
+                            v3 :: Vertex2 GLfloat,
+                            color :: Color3 GLfloat
+                            } deriving (Show)
+
+data TriObjectList = IORef [TriObject]
+
+randomObject :: StdGen -> TriObject
+randomObject gen = TriObject (Vertex2 (x + v1_X - 25) (y + v1_Y - 25))
+                   (Vertex2 (x + v2_X - 25) (y + v2_Y - 25))
+                   (Vertex2 (x + v3_X - 25) (y + v3_Y - 25))
+                   (Color3 ((c1 + 50) / 150.0) ((c2 + 50) / 150.0) ((c3 + 50) / 150.0) )
+  where
+    (initX, g1) = randomR (0,300) gen
+    (initY, g2) = randomR (0,300) g1
+    x = initX - 150
+    y = initY - 150
+    -- Get x verticies
+    (v1_X, g3) = randomR (0,50) g2
+    (v2_X, g4) = randomR (0,50) g3
+    (v3_X, g5) = randomR (0,50) g4
+    -- Get y verticies
+    (v1_Y, g6) = randomR (0,50) g5
+    (v2_Y, g7) = randomR (0,50) g6
+    (v3_Y, g8) = randomR (0,50) g7
+    -- Get color values
+    c1:c2:c3:[] = take 3 $ randomRs (0,100) g8
