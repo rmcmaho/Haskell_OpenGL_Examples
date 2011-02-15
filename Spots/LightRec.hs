8ad18764b526361bea7fae222d8660d14f37f0e7
Robbie McMahon <robbie@Mega-Oge.local>
2011-02-14 21:28:54 -0800

    Fixed .cabal file to prevent the following warning:
    		You are using the old package `base' version 3.x.
            Future GHC versions will not support base version 3.x. You
            should update your code to use the new base version 4.x.
    
    Also fixed some dumb changes I apparently made to the source code.

--- Spots/LightRec.hs
+++ Spots/LightRec.hs
@@ -10,21 +10,6 @@ import Graphics.Rendering.OpenGL
 import Data.IORef
 
 data LightStruct = LightStruct { amb :: Color4 GLfloat
-													,diff :: Color4 GLfloat
-													,spec :: Color4 GLfloat
-													,pos :: Vertex4 GLfloat
-													,spotDir :: Normal3 GLfloat
-													,spotExp :: GLfloat
-													,cutoff :: GLfloat
-													,atten :: (GLfloat, GLfloat, GLfloat)
-													,trans :: Vector3 GLfloat
-													,rot :: (GLfloat, GLfloat, GLfloat)
-													,swing :: (GLfloat, GLfloat, GLfloat)
-													,arc :: (GLfloat, GLfloat, GLfloat)
-													,arcIncr :: (GLfloat, GLfloat, GLfloat)
-													,lightNum :: GLsizei
-													}
-
                                ,diff :: Color4 GLfloat
                                ,spec :: Color4 GLfloat
                                ,pos :: Vertex4 GLfloat
@@ -39,7 +24,8 @@ data LightStruct = LightStruct { amb :: Color4 GLfloat
                                ,arcIncr :: (GLfloat, GLfloat, GLfloat)
                                ,lightNum :: GLsizei
                                }
-                   
+
+
 type LightList = IORef [LightStruct]
 
                  
